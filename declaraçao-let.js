bbc9abe5300026be8151a46d43609e11d4d528bd
pedrotiburskilima-arch <pedrotiburskilima@gmail.com>
2026-09-04 17:23:05 -0300

    add declaraçao-let.js

---
+++ declaraçao-let.js
@@ -0,0 +1,10 @@
+function exemploVAR(){
+    let x = 10;
+    if (true){
+        let x = 28 //mesma variavel x é redeclarada dentrop do bloco 
+        console.log(x); //28 (dentro do bloco)
+    }
+    console.log(x); // 28(o valor foi alterado)
+}
+
+exemploVAR();
