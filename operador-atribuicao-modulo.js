7d37165551feef9863daa3aeaa0a2eba3e0e5463
pedrotiburskilima-arch <pedrotiburskilima@gmail.com>
2026-09-11 17:38:29 -0300

    add operador-atribuicao-modulo.js

---
+++ operador-atribuicao-modulo.js
@@ -0,0 +1,7 @@
+let totalDoces = 10;
+let criancas = 3;
+
+//queremos saber quantos doces sobram apos dividir igualmente
+totalDoces %= criancas;
+
+console.log(totalDoces); // 1-> pois 10 dividido por 3 sobra 1
