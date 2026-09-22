ae7a943ebcfffae69065df31985263591b41b3e6
kaua-gabriel-franca <pedrotiburskilima@gmail.com>
2026-09-22 15:47:47 -0300

    add if.js

---
+++ if.js
@@ -0,0 +1,8 @@
+let hora = 15;
+
+if(hora < 12){
+    console.log("bom dia!")
+
+}
+// O restante do codigo continua independentemente se a condição for verdadeira ou não
+console.log("Dinheiro nao traz felicidade. Me dê o seu seja feliz ")
