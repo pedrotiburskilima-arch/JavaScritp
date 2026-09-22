e25f26ea20fd621ba526b5295212c453cef403ad
kaua-gabriel-franca <pedrotiburskilima@gmail.com>
2026-09-22 15:47:05 -0300

    add if-else2.js

---
+++ if-else2.js
@@ -0,0 +1,7 @@
+let diaDaSemana = "Quarta-feira"
+
+if(diaDaSemana === " sabado "  || diaDaSemana === "domingo"){
+    console.log("É fim de semana!");
+} else {
+    console.log("é dia de semana")
+}
